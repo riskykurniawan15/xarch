8e90cea7ca66fbafe7b4fee99c806fecfd26c980
Risky Kurniawan <risky.kurniawan@majoo.id>
2022-04-05 04:53:52 +0700

    create repository

--- domain/repository/user.go
+++ domain/repository/user.go
@@ -1,1 +1,19 @@
 package repository
+
+import (
+	"gorm.io/gorm"
+)
+
+type UserRepo struct {
+	DB *gorm.DB
+}
+
+func NewUserRepo(DB *gorm.DB) *UserRepo {
+	return &UserRepo{
+		DB,
+	}
+}
+
+func (DB UserRepo) SelectUser() {
+
+}
