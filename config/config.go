4938d3f4a34e37d8cea8774951b2745b3e48d5e4
Risky Kurniawan <risky.kurniawan@majoo.id>
2022-04-04 08:11:58 +0700

    add config db

--- config/config.go
+++ config/config.go
@@ -8,11 +8,20 @@ import (
 
 type Config struct {
 	Http HttpServer
+	DB   DBServer
 }
 
 type HttpServer struct {
 	Server string
 	Port   string
+}
+
+type DBServer struct {
+	DB_USER   string
+	DB_PASS   string
+	DB_SERVER string
+	DB_PORT   string
+	DB_NAME   string
 }
 
 func Configuration() Config {
@@ -25,6 +34,12 @@ func Configuration() Config {
 
 	cfg.Http.Server = os.Getenv("SERVER")
 	cfg.Http.Port = os.Getenv("PORT")
+
+	cfg.DB.DB_USER = os.Getenv("DB_USER")
+	cfg.DB.DB_PASS = os.Getenv("DB_PASS")
+	cfg.DB.DB_SERVER = os.Getenv("DB_SERVER")
+	cfg.DB.DB_PORT = os.Getenv("DB_PORT")
+	cfg.DB.DB_NAME = os.Getenv("DB_NAME")
 
 	return cfg
 }
