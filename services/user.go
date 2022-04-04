63bc48ceff3cbeb9fe4d30e06e2dba24b6d90a68
Risky Kurniawan <risky.kurniawan@majoo.id>
2022-04-05 04:54:18 +0700

    create user service

--- services/user.go
+++ services/user.go
@@ -1,1 +1,19 @@
 package services
+
+import (
+	"github.com/riskykurniawan15/xarch/domain/repository"
+)
+
+type UserService struct {
+	UserRepo *repository.UserRepo
+}
+
+func NewUserService(UR *repository.UserRepo) *UserService {
+	return &UserService{
+		UR,
+	}
+}
+
+func (US UserService) GetListUser() {
+
+}
