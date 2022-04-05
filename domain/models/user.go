407cfefffe773e55764a1f708a84bbeb70295458
Risky Kurniawan <risky.kurniawan@majoo.id>
2022-04-05 08:56:17 +0700

    add tag json

--- domain/models/user.go
+++ domain/models/user.go
@@ -3,11 +3,11 @@ package models
 const UserTable = "users"
 
 type User struct {
-	ID      uint   `gorm:"column:id;primaryKey"`
-	Name    string `gorm:"column:name"`
-	Email   string `gorm:"column:email"`
-	Contact string `gorm:"column:contact"`
-	Gender  string `gorm:"column:gender"`
+	ID      uint   `gorm:"column:id;primaryKey;autoIncrement" json:"id"`
+	Name    string `gorm:"column:name"                        json:"name"`
+	Email   string `gorm:"column:email"                       json:"email"`
+	Contact string `gorm:"column:contact"                     json:"contact"`
+	Gender  string `gorm:"column:gender"                      json:"gender"`
 }
 
 func (User) TableName() string {
