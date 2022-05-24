a05dca547fce52e5fbb4607e7255788b12b7420c
Risky Kurniawan <risky.kurniawan@majoo.id>
2022-05-24 11:25:55 +0700

    remove logger

--- main.go
+++ main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"github.com/riskykurniawan15/xarch/exec/arch"
-	"github.com/riskykurniawan15/xarch/logger"
 )
 
 func main() {
-	logger := logger.NewLogger()
-	logger.Info("Starting Application")
 	arch.Startup()
 }
