7d25665c9a2cdca14a2be3e0f33615c4081700cc
Risky Kurniawan <risky.kurniawan@majoo.id>
2022-05-25 20:54:52 +0700

    rollback inprovement handler

--- interfaces/http/handlers/alquran.go
+++ interfaces/http/handlers/alquran.go
@@ -20,49 +20,41 @@ func NewAlquranHandlers(AS *services.AlquranService) *AlquranHandler {
 }
 
 func (handler AlquranHandler) ListChapter(ctx echo.Context) error {
-	var responseData entities.Response
-
 	data, err := handler.AlquranService.GetListChapter(ctx.Request().Context())
 	if err != nil {
-		responseData = entities.ResponseFormater(http.StatusBadGateway, map[string]interface{}{
+		return ctx.JSON(http.StatusBadGateway, entities.ResponseFormater(http.StatusBadGateway, map[string]interface{}{
 			"error": err,
-		})
-	} else {
-		responseData = entities.ResponseFormater(http.StatusOK, map[string]interface{}{
-			"data": data,
-		})
+		}))
 	}
 
-	return ctx.JSON(responseData.Status, responseData)
+	return ctx.JSON(http.StatusOK, entities.ResponseFormater(http.StatusOK, map[string]interface{}{
+		"data": data,
+	}))
 }
 
 func (handler AlquranHandler) DetailChapter(ctx echo.Context) error {
-	var responseData entities.Response
-
 	ID, err := strconv.Atoi(ctx.Param("ID"))
 
 	if err != nil {
-		responseData = entities.ResponseFormater(http.StatusBadRequest, map[string]interface{}{
+		return ctx.JSON(http.StatusBadRequest, entities.ResponseFormater(http.StatusBadRequest, map[string]interface{}{
 			"error": err,
-		})
+		}))
 	}
 
 	if ID < 1 || ID > 114 {
-		responseData = entities.ResponseFormater(http.StatusBadRequest, map[string]interface{}{
+		return ctx.JSON(http.StatusBadRequest, entities.ResponseFormater(http.StatusBadRequest, map[string]interface{}{
 			"error": "Chapter min 1 and max 114",
-		})
+		}))
 	}
 
 	data, err := handler.AlquranService.GetDetailChapter(ctx.Request().Context(), ID)
 	if err != nil {
-		responseData = entities.ResponseFormater(http.StatusBadGateway, map[string]interface{}{
+		return ctx.JSON(http.StatusBadGateway, entities.ResponseFormater(http.StatusBadGateway, map[string]interface{}{
 			"error": err,
-		})
-	} else {
-		responseData = entities.ResponseFormater(http.StatusOK, map[string]interface{}{
-			"data": data,
-		})
+		}))
 	}
 
-	return ctx.JSON(responseData.Status, responseData)
+	return ctx.JSON(http.StatusOK, entities.ResponseFormater(http.StatusOK, map[string]interface{}{
+		"data": data,
+	}))
 }
