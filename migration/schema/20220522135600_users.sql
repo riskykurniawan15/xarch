df71f67662dc4211ba1ab4dac9b726bf9cfccce2
Risky Kurniawan <risky.kurniawan@majoo.id>
2022-05-26 13:54:38 +0700

    update schema

--- migration/schema/20220522135600_users.sql
+++ migration/schema/20220522135600_users.sql
@@ -3,8 +3,8 @@
 CREATE TABLE `users` (
   `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `contact` varchar(15) NOT NULL,
+  `email` varchar(100) NOT NULL UNIQUE,
+  `password` varchar(100) NOT NULL,
   `gender` enum('male','female') NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
