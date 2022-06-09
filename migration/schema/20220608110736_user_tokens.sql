d80721aa26102b9e5ccd2110cac3a425fcc27f51
Risky Kurniawan <risky.kurniawan@majoo.id>
2022-06-09 19:13:19 +0700

    add auto increment in id

---
+++ migration/schema/20220608110736_user_tokens.sql
@@ -0,0 +1,19 @@
+--Up
+CREATE TABLE `user_tokens` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `method` enum('verified','forgot') NOT NULL,
+  `token` varchar(100) UNIQUE NOT NULL,
+  `expired` TIMESTAMP NULL,
+  `created_at` TIMESTAMP NULL,
+  `updated_at` TIMESTAMP NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+    ON UPDATE CASCADE ON DELETE CASCADE,
+  INDEX (method, token),
+  INDEX (user_id, method, token)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+--EndUp
+--Down
+DROP TABLE IF EXISTS `user_tokens`;
+--EndDown
