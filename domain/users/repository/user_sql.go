edf76a2f51af1b4adbdc015e9ed91e0ac2b03b68
Risky Kurniawan <risky.kurniawan@majoo.id>
2022-06-19 14:02:41 +0700

    refactor user sql

---
+++ domain/users/repository/user_sql.go
@@ -0,0 +1,78 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/riskykurniawan15/xarch/domain/users/models"
+	"github.com/riskykurniawan15/xarch/helpers/jwt"
+)
+
+func (repo UserRepo) GenerateTokenUser(ctx context.Context, user *models.User) (string, error) {
+	NewJwt := jwt.NewJwtToken(repo.cfg)
+
+	pyld := &jwt.JwtCustomClaims{
+		ID:    user.ID,
+		Email: user.Email,
+	}
+
+	token, err := NewJwt.GenerateToken(pyld)
+	if err != nil {
+		return "", err
+	}
+
+	return token, nil
+}
+
+func (repo UserRepo) InsertUser(ctx context.Context, user *models.User) (*models.User, error) {
+	if err := repo.DB.
+		WithContext(ctx).
+		Create(user).
+		Error; err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (repo UserRepo) SelectUserDetailByEmail(ctx context.Context, user *models.User) (*models.User, error) {
+	var model *models.User
+
+	result := repo.DB.
+		WithContext(ctx).
+		Model(&models.User{}).
+		Where("email = ?", user.Email).
+		First(&model)
+
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return model, nil
+}
+
+func (repo UserRepo) SelectUserDetail(ctx context.Context, user *models.User) (*models.User, error) {
+	var model *models.User
+
+	result := repo.DB.
+		WithContext(ctx).
+		Model(&models.User{}).
+		Where("id = ?", user.ID).
+		First(&model)
+
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return model, nil
+}
+
+func (repo UserRepo) InsertTokenEmailVerfied(ctx context.Context, token *models.UserToken) (*models.UserToken, error) {
+	if err := repo.DB.
+		WithContext(ctx).
+		Create(token).
+		Error; err != nil {
+		return nil, err
+	}
+
+	return token, nil
+}
