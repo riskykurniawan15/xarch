e4b4f95d35525a2f24c4b6868fe89b79fd855565
Risky Kurniawan <risky.kurniawan@majoo.id>
2022-07-02 12:10:25 +0700

    update configuration

--- config/config.go
+++ config/config.go
@@ -2,11 +2,9 @@ package config
 
 import (
 	"fmt"
-	"os"
-	"strconv"
 	"strings"
 
-	"github.com/joho/godotenv"
+	"github.com/riskykurniawan15/xarch/helpers/env"
 	"github.com/riskykurniawan15/xarch/logger"
 )
 
@@ -74,101 +72,99 @@ type Other struct {
 }
 
 func Configuration() Config {
+
 	log.Info("Load all configuration")
-	var cfg Config
-
-	err := godotenv.Load(".env")
+	err := env.LoadEnv(".env")
 	if err != nil {
-		log.PanicW("Error loading .env file", err)
-		panic("Error loading .env file")
+		log.ErrorW("error read .env file", err.Error())
 	}
 
-	cfg.Http.Server = os.Getenv("SERVER")
-	cfg.Http.Port, err = strconv.Atoi(os.Getenv("PORT"))
-	if err != nil {
-		log.PanicW("PORT must be number", err)
-		panic("PORT must be number")
-	}
-	if strings.ToLower(os.Getenv("USING_SECURE")) == "true" {
-		cfg.Http.URL = "https://" + cfg.Http.Server
-	} else {
-		cfg.Http.URL = "http://" + cfg.Http.Server
-	}
-
-	if cfg.Http.Port != 0 {
-		cfg.Http.URL += fmt.Sprintf(":%d", cfg.Http.Port)
-	}
-	cfg.Http.URL += "/"
-
-	cfg.DB.DB_DRIVER = strings.ToLower(os.Getenv("DB_DRIVER"))
-	cfg.DB.DB_USER = os.Getenv("DB_USER")
-	cfg.DB.DB_PASS = os.Getenv("DB_PASS")
-	cfg.DB.DB_SERVER = os.Getenv("DB_SERVER")
-	cfg.DB.DB_PORT, err = strconv.Atoi(os.Getenv("DB_PORT"))
-	if err != nil {
-		log.PanicW("DB_PORT must be number", err)
-		panic("DB_PORT must be number")
-	}
-	cfg.DB.DB_NAME = os.Getenv("DB_NAME")
-	cfg.DB.DB_MAX_IDLE_CON, err = strconv.Atoi(os.Getenv("DB_MAX_IDLE_CON"))
-	if err != nil {
-		log.PanicW("DB_MAX_IDLE_CON must be number", err)
-		panic("DB_MAX_IDLE_CON must be number")
-	}
-	cfg.DB.DB_MAX_OPEN_CON, err = strconv.Atoi(os.Getenv("DB_MAX_OPEN_CON"))
-	if err != nil {
-		log.PanicW("DB_MAX_OPEN_CON must be number", err)
-		panic("DB_MAX_OPEN_CON must be number")
-	}
-	cfg.DB.DB_MAX_LIFE_TIME, err = strconv.Atoi(os.Getenv("DB_MAX_LIFE_TIME"))
-	if err != nil {
-		log.PanicW("DB_MAX_LIFE_TIME must be number", err)
-		panic("DB_MAX_LIFE_TIME must be number")
-	}
-
-	cfg.RDB.RDB_ADDRESS = os.Getenv("RDB_ADDRESS")
-	cfg.RDB.RDB_PORT, err = strconv.Atoi(os.Getenv("RDB_PORT"))
-	if err != nil {
-		log.PanicW("RDB_PORT must be number", err)
-		panic("RDB_PORT must be number")
-	}
-	cfg.RDB.RDB_USER = os.Getenv("RDB_USER")
-	cfg.RDB.RDB_PASS = os.Getenv("RDB_PASS")
-	cfg.RDB.RDB_DB_DEFAULT, err = strconv.Atoi(os.Getenv("RDB_DB_DEFAULT"))
-	if err != nil {
-		log.PanicW("RDB_DB_DEFAULT must be number", err)
-		panic("RDB_DB_DEFAULT must be number")
-	}
-
-	cfg.Email.EMAIL_HOST = os.Getenv("EMAIL_HOST")
-	cfg.Email.EMAIL_PORT, err = strconv.Atoi(os.Getenv("EMAIL_PORT"))
-	if err != nil {
-		log.PanicW("EMAIL_PORT must be number", err)
-		panic("EMAIL_PORT must be number")
-	}
-	cfg.Email.EMAIL_NAME = os.Getenv("EMAIL_NAME")
-	cfg.Email.EMAIL_EMAIL = os.Getenv("EMAIL_EMAIL")
-	cfg.Email.EMAIL_PASSWORD = os.Getenv("EMAIL_PASSWORD")
-
-	cfg.KAFKA.KAFKA_SERVER = os.Getenv("KAFKA_SERVER")
-	cfg.KAFKA.KAFKA_PORT, err = strconv.Atoi(os.Getenv("KAFKA_PORT"))
-	if err != nil {
-		log.PanicW("KAFKA_PORT must be number", err)
-		panic("KAFKA_PORT must be number")
-	}
-	cfg.KAFKA.KAFKA_CONSUMER_GROUP = os.Getenv("KAFKA_CONSUMER_GROUP")
-	cfg.KAFKA.TOPIC_EMAIL_VERIFIED = os.Getenv("TOPIC_EMAIL_VERIFIED")
-	cfg.KAFKA.TOPIC_PASS_FORGOT = os.Getenv("TOPIC_PASS_FORGOT")
-
-	cfg.OTHER.AlQuranAPI = os.Getenv("ALQURAN_API")
-
-	cfg.JWT.SecretKey = os.Getenv("JWT_SECRET_KEY")
-	cfg.JWT.Expired, err = strconv.Atoi(os.Getenv("JWT_EXPIRED"))
-	if err != nil {
-		log.PanicW("JWT_EXPIRED must be number", err)
-		panic("JWT_EXPIRED must be number")
+	cfg := Config{
+		Http:  loadHttpServer(),
+		DB:    loadDBServer(),
+		RDB:   loadRDBServer(),
+		Email: loadEmailSender(),
+		KAFKA: loadKafkaConfig(),
+		JWT:   loadJWTConfig(),
+		OTHER: loadOther(),
 	}
 
 	log.Info("Success for load all configuration")
+
 	return cfg
+}
+
+func loadHttpServer() HttpServer {
+	var cfg HttpServer
+
+	cfg.Server = env.GetString("SERVER")
+	cfg.Port = env.GetInt("PORT")
+	if env.GetBoolean("USING_SECURE") {
+		cfg.URL = "https://" + cfg.Server
+	} else {
+		cfg.URL = "http://" + cfg.Server
+	}
+	if cfg.Port != 0 {
+		cfg.URL += fmt.Sprintf(":%d", cfg.Port)
+	}
+	cfg.URL += "/"
+
+	return cfg
+}
+
+func loadDBServer() DBServer {
+	return DBServer{
+		DB_DRIVER:        strings.ToLower(env.GetString("DB_DRIVER")),
+		DB_USER:          env.GetString("DB_USER"),
+		DB_PASS:          env.GetString("DB_PASS"),
+		DB_SERVER:        env.GetString("DB_SERVER"),
+		DB_PORT:          env.GetInt("DB_PORT"),
+		DB_NAME:          env.GetString("DB_NAME"),
+		DB_MAX_IDLE_CON:  env.GetInt("DB_MAX_IDLE_CON"),
+		DB_MAX_OPEN_CON:  env.GetInt("DB_MAX_OPEN_CON"),
+		DB_MAX_LIFE_TIME: env.GetInt("DB_MAX_LIFE_TIME"),
+	}
+}
+
+func loadRDBServer() RDBServer {
+	return RDBServer{
+		RDB_ADDRESS:    env.GetString("RDB_ADDRESS"),
+		RDB_PORT:       env.GetInt("RDB_PORT"),
+		RDB_USER:       env.GetString("RDB_USER"),
+		RDB_PASS:       env.GetString("RDB_PASS"),
+		RDB_DB_DEFAULT: env.GetInt("RDB_DB_DEFAULT"),
+	}
+}
+
+func loadEmailSender() EmailSender {
+	return EmailSender{
+		EMAIL_HOST:     env.GetString("EMAIL_HOST"),
+		EMAIL_PORT:     env.GetInt("EMAIL_PORT"),
+		EMAIL_NAME:     env.GetString("EMAIL_NAME"),
+		EMAIL_EMAIL:    env.GetString("EMAIL_EMAIL"),
+		EMAIL_PASSWORD: env.GetString("EMAIL_PASSWORD"),
+	}
+}
+
+func loadKafkaConfig() KafkaConfig {
+	return KafkaConfig{
+		KAFKA_SERVER:         env.GetString("KAFKA_SERVER"),
+		KAFKA_PORT:           env.GetInt("KAFKA_PORT"),
+		KAFKA_CONSUMER_GROUP: env.GetString("KAFKA_CONSUMER_GROUP"),
+		TOPIC_EMAIL_VERIFIED: env.GetString("TOPIC_EMAIL_VERIFIED"),
+		TOPIC_PASS_FORGOT:    env.GetString("TOPIC_PASS_FORGOT"),
+	}
+}
+
+func loadJWTConfig() JWTConfig {
+	return JWTConfig{
+		SecretKey: env.GetString("JWT_SECRET_KEY"),
+		Expired:   env.GetInt("JWT_EXPIRED"),
+	}
+}
+
+func loadOther() Other {
+	return Other{
+		AlQuranAPI: env.GetString("ALQURAN_API"),
+	}
 }
---
+++ helpers/env/env.go
@@ -0,0 +1,32 @@
+package env
+
+import (
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadEnv(file string) error {
+	return godotenv.Load(file)
+}
+
+func GetString(val string) string {
+	return os.Getenv(val)
+}
+
+func GetInt(val string) int {
+	result, err := strconv.Atoi(os.Getenv(val))
+	if err != nil {
+		return 0
+	}
+	return result
+}
+
+func GetBoolean(val string) bool {
+	result, err := strconv.ParseBool(os.Getenv(val))
+	if err != nil {
+		return false
+	}
+	return result
+}
